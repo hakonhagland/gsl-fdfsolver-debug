17177af4b343b0aab1a2c06506c5368924181e8e
Håkon Hægland <hakon.hagland@gmail.com>
2019-08-23 16:35:15 +0200

    Fixed typo.

--- README.md
+++ README.md
@@ -6,7 +6,7 @@
 Output should be like:
 
 ```
-The input parameter 'T' should be equal to the global symbol the  'gsl_root_fdfsolver_newton'... checking:
+The input parameter 'T' should be equal to the global symbol 'gsl_root_fdfsolver_newton'... checking:
   T = 0x7fe1eb744030
   gsl_root_fdfsolver_newton = 0x7fe1eb743e10
   --> no match!
--- lib/fdfsolver.c
+++ lib/fdfsolver.c
@@ -5,7 +5,7 @@ gsl_root_fdfsolver *
 gsl_root_fdfsolver_alloc (const gsl_root_fdfsolver_type * T)
 {
     printf("The input parameter 'T' should be equal to the global symbol "
-        "the  'gsl_root_fdfsolver_newton'... checking:\n");
+        "'gsl_root_fdfsolver_newton'... checking:\n");
     printf("  T = %p\n", T);
     printf("  gsl_root_fdfsolver_newton = %p\n", gsl_root_fdfsolver_newton);
     if ( T != gsl_root_fdfsolver_newton ) {
